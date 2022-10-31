79f1b5cc726f2aa66f4a3d5188d032c318f1fde7
MaVi21 <35688602+MaVi21@users.noreply.github.com>
2022-10-31 13:34:28 +0200

    18-10 lesson

---
+++ OGL 2022 Coders/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public string sceneToLoad;
+    private Text nameText;
+    private Text moneyText;
+    
+    void Start()
+    {
+        nameText = GameObject.Find("SceneNameText").GetComponent<Text>();
+        nameText.text = "Scene: " + SceneManager.GetActiveScene().name;
+
+        Debug.Log("Scene " + SceneManager.GetActiveScene().name + " loaded");
+        Debug.Log("Player's money: " + PlayerInfo.Money);
+
+        moneyText = GameObject.Find("MoneyText").GetComponent<Text>();
+        moneyText.text = "Player's money: " + PlayerInfo.Money;
+    }
+
+    public void OnMoneyButtonClicked()
+    {
+        PlayerInfo.Money++;
+        Debug.Log("Money updated to: " + PlayerInfo.Money);
+        moneyText.text = "Player's money: " + PlayerInfo.Money;
+    }
+
+    public void OnLoadSceneButtonClicked()
+    {
+       // Scene scene = SceneManager.GetActiveScene();
+
+       // if(scene.name == "StaticClass1")
+            SceneManager.LoadScene(sceneToLoad);
+       // else
+         //   SceneManager.LoadScene("StaticClass1");
+    }
+}
